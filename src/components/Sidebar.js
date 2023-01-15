cfbe35c385baec0232b47b5d187aca2b64268d90
Ahmed <ahmed_hany19@outlook.com>
2023-01-16 01:57:48 +0200

    Adding "Modal" page & component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,6 +5,7 @@ function Sidebar() {
     { label: 'Dropdown', path: '/' },
     { label: 'Accordion', path: '/accordion' },
     { label: 'Buttons', path: '/buttons' },
+    { label: 'Modal', path: '/modal' },
   ];
 
   const renderedLinks = links.map((link) => {
