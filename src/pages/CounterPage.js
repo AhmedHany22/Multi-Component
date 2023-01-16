812c2ee0aa488b28eefac74ed4a1b367e53a3124
Ahmed <ahmed_hany19@outlook.com>
2023-01-16 02:05:33 +0200

    Creating counter page & custom hook

---
+++ src/pages/CounterPage.js
@@ -0,0 +1,15 @@
+import Button from '../components/Button';
+import useCounter from '../hooks/use-counter';
+
+function CounterPage({ initialCount }) {
+  const { count, increment } = useCounter(initialCount);
+
+  return (
+    <div>
+      <h1>Count is {count}</h1>
+      <Button onClick={increment}>Increment</Button>
+    </div>
+  );
+}
+
+export default CounterPage;
