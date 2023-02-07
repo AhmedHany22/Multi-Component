81d61e0a74b8fde07af7f19fd2957c22f090b65b
Ahmed <ahmed_hany19@outlook.com>
2023-02-07 18:33:48 +0200

    Add Modal

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import { GoChevronDown, GoChevronLeft } from 'react-icons/go';
+import { useState } from "react";
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
 
 function Accordion({ items }) {
   const [expandedIndex, setExpandedIndex] = useState(-1);
@@ -15,20 +15,13 @@ function Accordion({ items }) {
   };
 
   const renderedItems = items.map((item, index) => {
-    const isExpanded = index === expandedIndex;
+    const isExpanded = index == expandedIndex;
 
-    const icon = (
-      <span className="text-2xl">
-        {isExpanded ? <GoChevronDown /> : <GoChevronLeft />}
-      </span>
-    );
+    const icon = <span className="text-2xl">{isExpanded ? <GoChevronDown /> : <GoChevronLeft />}</span>;
 
     return (
       <div key={item.id}>
-        <div
-          className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer"
-          onClick={() => handleClick(index)}
-        >
+        <div className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer" onClick={() => handleClick(index)}>
           {item.label}
           {icon}
         </div>
